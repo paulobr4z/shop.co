bf309e2ca2325bf74ff596e482234ae1ee0823b2
paulobr4z <paulobrazaraujo@gmail.com>
2024-09-13 23:45:40 -0300

    feat: create hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,55 @@
+import { Button } from '@/components/Button'
 import { Header } from '@/components/Header'
+import { Plus } from 'lucide-react'
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <h1 className="font-body font-normal">Regular</h1>
-      <h1 className="font-body font-medium">Medium</h1>
-      <h1 className="font-body font-bold">Bold</h1>
-      <h1 className="font-display text-6xl">
-        FIND CLOTHES THAT MATCHES YOUR STYLE
-      </h1>
+      <section className="bg-[#F0F0F0] bg-[url('../assets/images/bg-hero.svg')] bg-center bg-no-repeat">
+        <div className="mx-auto my-0 max-w-app py-[116px]">
+          <h1 className="max-w-[580px] font-display text-[64px] leading-[64px]">
+            FIND CLOTHES THAT MATCHES YOUR STYLE
+          </h1>
+          <p className="mt-8 max-w-[545px] font-body leading-[22px] opacity-60">
+            Browse through our diverse range of meticulously crafted garments,
+            designed to bring out your individuality and cater to your sense of
+            style.
+          </p>
+          <Button className="mt-8" variant="primary" maxWidth="210px">
+            Shop Now
+          </Button>
+          <div className="mt-12 flex">
+            <div className="border-r border-[#0000001a] pr-8">
+              <span className="flex items-center">
+                <h3 className="font-body text-[40px] font-bold leading-[48px]">
+                  200
+                </h3>
+                <Plus strokeWidth={3} />
+              </span>
+              <p className="text-black opacity-60">International Brands</p>
+            </div>
+            <div className="px-8">
+              <span className="flex items-center">
+                <h3 className="font-body text-[40px] font-bold leading-[48px]">
+                  2,000
+                </h3>
+                <Plus strokeWidth={3} />
+              </span>
+              <p className="text-black opacity-60">High-Quality Products</p>
+            </div>
+            <div className="border-l border-[#0000001a] pl-8">
+              <span className="flex items-center">
+                <h3 className="font-body text-[40px] font-bold leading-[48px]">
+                  30,000
+                </h3>
+                <Plus strokeWidth={3} />
+              </span>
+              <p className="text-black opacity-60">Happy Customers</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,39 @@
+import { ComponentProps } from 'react'
+import { tv, VariantProps } from 'tailwind-variants'
+
+const button = tv({
+  base: ['w-full rounded-full py-4 text-base font-medium outline-none'],
+
+  variants: {
+    variant: {
+      primary: 'bg-black text-white hover:opacity-95',
+      secondary: 'bg-white text-black hover:bg-[#F0F0F0]',
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+  },
+})
+
+export type ButtonProps = ComponentProps<'button'> &
+  VariantProps<typeof button> & {
+    maxWidth?: string
+  }
+
+export function Button({
+  variant,
+  className,
+  maxWidth,
+  ...props
+}: ButtonProps) {
+  const maxWidthStyle = maxWidth ? { maxWidth } : undefined
+
+  return (
+    <button
+      {...props}
+      className={button({ variant, className })}
+      style={maxWidthStyle}
+    />
+  )
+}
